20e3d4de41f40164af8b72dc6c4c73266ec26acd
dlyrsk <dlyrsk@gmail.com>
2023-10-18 01:18:09 +1030

    Initialize package

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pm4py
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def run_analyzer():
+    dfg, start_activities, end_activities = pm4py.discover_dfg(log)
+    pm4py.view_dfg(dfg, start_activities, end_activities, format='jpg')
+    tree = pm4py.discover_process_tree_inductive(log)
+    pm4py.view_process_tree(tree, format='jpg')
+    net, initial_marking, final_marking = pm4py.convert_to_petri_net(tree)
+    pm4py.view_petri_net(net, initial_marking, final_marking, format='jpg')
+    bpmn = pm4py.convert_to_bpmn(net, initial_marking, final_marking)
+    pm4py.view_bpmn(bpmn, format='jpg')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    run_analyzer()
